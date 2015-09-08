d3a40b14b179a9deea75ace974167ea6fae67347
littleqz <qizhihere@gmail.com>
2015-09-09 01:00:09 +0800

    modify time_ago and add time_compare

--- helpers.php
+++ helpers.php
@@ -1,44 +1,40 @@
+<?php
+
+// format time into N years/months/weeks/days ago
 if ( ! function_exists('time_ago')) {
-    function time_ago($timestamp, $lang='cn')
+    function time_ago($time, $now=null)
     {
-        $msg = [
-            'cn' => [
-                'day' => '天前',
-                'hour' => '小时前',
-                'minute' => '分钟前',
-                'second' => '秒前',
-            ],
-            'en' => [
-                'day' => ' days ago',
-                'hour' => ' hours ago',
-                'minute' => ' minutes ago',
-                'second' => ' seconds ago',
-            ]
+        $now = $now ? (new Datetime($now)) : (new Datetime);
+        $time = new Datetime($time);
+        $diff = $now->diff($time);
+
+        $units = [
+            '年前'   => 'y',
+            '月前'   => 'm',
+            '周前'   => 'w',
+            '天前'   => 'd',
+            '小时前' => 'h',
+            '分钟前' => 'i',
+            '秒前'   => 's',
         ];
 
-        // convert to timestamp
-        if (is_string($timestamp)) {
-            $timestamp = strtotime($timestamp);
+        foreach ($units as $unit => $k) {
+            if (@$diff->$k) {
+                return $diff->$k . $unit;
+            }
         }
+    }
+}
 
-        $now = time();
-        $interval = $now - $timestamp;
 
-        $days = intval($interval / (24 * 3600));
-        if ($days > 0) {
-            return $days . $msg[$lang]['day'];
-        }
+// compare two timestamp like `strcmp`
+if ( ! function_exists('time_compare')) {
+    function time_compare($time, $time_str)
+    {
+        $to_timestamp = function ($time) {
+            return is_string($time) ? strtotime($time) : (int)$time;
+        };
 
-        $hours = intval($interval / 3600);
-        if ($hours > 0) {
-            return $hours . $msg[$lang]['hour'];
-        }
-
-        $minutes = intval($interval / 60);
-        if ($minutes > 0) {
-            return $minutes . $msg[$lang]['minute'];
-        }
-
-        return $interval. $msg[$lang]['second'];
+        return $to_timestamp($time) - $to_timestamp($time_str);
     }
 }
